c5180ed85d5aeee50129671bb9d66397fd61434f
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-10-03 10:16:15 +0200

    Add first commit

---
+++ edu-backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const healthcheck = require('healthcheck')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+app.use('/health', require('./routes/healthcheck.js'));
+app.use('/user', require('./routes/user.js'));
+
+app.get("/", (req ,res) => {
+   headers={"cache-control":  "no-cache"}
+   body={"status": "available"}
+   res.status(200).json(body)
+})
+
+app.listen(PORT , ()=>{
+   console.log(`STARTED LISTENING ON PORT ${PORT}`)
+});
